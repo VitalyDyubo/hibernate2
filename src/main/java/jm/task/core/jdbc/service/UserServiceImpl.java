c39c7db957ac9bdc0bc80b50cbf993c37750c453
VitalyDyubo <123413746+VitalyDyubo@users.noreply.github.com>
2023-03-28 01:17:08 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
@@ -11,8 +12,8 @@ import java.sql.Statement;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-    private final UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    private final UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    private final UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
 
     public void createUsersTable() {
